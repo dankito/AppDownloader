ce4afa3c0dc7850943751f05bfa349f50b81ec3c
dankito <dankito@user.noreply.github.com>
2016-11-14 23:31:18 +0100

    Implemented FullscreenDialog

---
+++ AppDownloaderAndroid/src/main/java/net/dankito/appdownloader/dialogs/FullscreenDialog.java
@@ -0,0 +1,80 @@
+package net.dankito.appdownloader.dialogs;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+
+import net.dankito.appdownloader.R;
+
+/**
+ * Created by ganymed on 06/11/16.
+ */
+
+public abstract class FullscreenDialog extends Dialog {
+
+  protected AppCompatActivity activity;
+
+  protected Toolbar toolbar;
+
+
+  public FullscreenDialog(AppCompatActivity context) {
+    super(context, R.style.FullscreenDialog);
+
+    this.activity = context;
+  }
+
+
+  protected abstract int getLayoutId();
+
+  protected abstract void setupUi(View rootView);
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    requestWindowFeature(Window.FEATURE_ACTION_BAR);
+    requestWindowFeature(Window.FEATURE_CONTEXT_MENU);
+
+    LayoutInflater layoutInflater = activity.getLayoutInflater();
+    View rootView = layoutInflater.inflate(getLayoutId(), null);
+    addContentView(rootView, (new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)));
+
+    setupToolbar(rootView);
+
+    setupUi(rootView);
+  }
+
+  protected void setupToolbar(View rootView) {
+    toolbar = (Toolbar)rootView.findViewById(R.id.toolbar);
+    toolbar.setTitle("");
+
+    activity.setSupportActionBar(toolbar);
+
+    ActionBar actionBar = activity.getSupportActionBar();
+    if(actionBar != null) {
+      actionBar.setDisplayHomeAsUpEnabled(true);
+      actionBar.setHomeButtonEnabled(true);
+    }
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    int id = item.getItemId();
+
+    if (id == android.R.id.home) {
+      hide();
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+
+}
