6e7bdc0b2d3091ece59dddf4d46c9d46271a2f3f
dankito <dankito@user.noreply.github.com>
2016-11-19 15:47:02 +0100

    Added AppPackageVerificationResult

---
+++ AppDownloaderLib/src/main/java/net/dankito/appdownloader/util/app/AppPackageVerificationResult.java
@@ -0,0 +1,104 @@
+package net.dankito.appdownloader.util.app;
+
+import net.dankito.appdownloader.app.AppDownloadInfo;
+
+/**
+ * Created by ganymed on 19/11/16.
+ */
+
+public class AppPackageVerificationResult {
+
+  protected AppDownloadInfo downloadInfo;
+
+  protected boolean isCompletelyDownloaded;
+
+  protected boolean isPackageNameCorrect;
+
+  protected boolean isVersionCorrect;
+
+  protected boolean isFileChecksumCorrect;
+
+  protected boolean isAppSignatureCorrect;
+
+  protected String errorMessage;
+
+
+  public AppPackageVerificationResult(AppDownloadInfo downloadInfo) {
+    this.downloadInfo = downloadInfo;
+  }
+
+  public AppPackageVerificationResult(AppDownloadInfo downloadInfo, String errorMessage) {
+    this(downloadInfo);
+    this.errorMessage = errorMessage;
+  }
+
+
+  public boolean wasVerificationSuccessful() {
+    return isCompletelyDownloaded() && isPackageNameCorrect() && isVersionCorrect() &&
+        isFileChecksumCorrect() && isAppSignatureCorrect();
+  }
+
+
+  public AppDownloadInfo getDownloadInfo() {
+    return downloadInfo;
+  }
+
+  public boolean isCompletelyDownloaded() {
+    return isCompletelyDownloaded;
+  }
+
+  public void setCompletelyDownloaded(boolean completelyDownloaded) {
+    isCompletelyDownloaded = completelyDownloaded;
+  }
+
+  public boolean isPackageNameCorrect() {
+    return isPackageNameCorrect;
+  }
+
+  public void setPackageNameCorrect(boolean packageNameCorrect) {
+    isPackageNameCorrect = packageNameCorrect;
+  }
+
+  public boolean isVersionCorrect() {
+    return isVersionCorrect;
+  }
+
+  public void setVersionCorrect(boolean versionCorrect) {
+    isVersionCorrect = versionCorrect;
+  }
+
+  public boolean isFileChecksumCorrect() {
+    return isFileChecksumCorrect;
+  }
+
+  public void setFileChecksumCorrect(boolean fileChecksumCorrect) {
+    isFileChecksumCorrect = fileChecksumCorrect;
+  }
+
+  public boolean isAppSignatureCorrect() {
+    return isAppSignatureCorrect;
+  }
+
+  public void setAppSignatureCorrect(boolean appSignatureCorrect) {
+    isAppSignatureCorrect = appSignatureCorrect;
+  }
+
+  public String getErrorMessage() {
+    return errorMessage;
+  }
+
+  public void setErrorMessage(String errorMessage) {
+    this.errorMessage = errorMessage;
+  }
+
+
+  @Override
+  public String toString() {
+    if(wasVerificationSuccessful()) {
+      return "Successfully verified";
+    }
+
+    return errorMessage;
+  }
+
+}
