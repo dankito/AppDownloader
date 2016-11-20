c58b38859f7e65acb2150c22a7dd076b8b377976
dankito <dankito@user.noreply.github.com>
2016-11-20 22:55:36 +0100

    Fixed algorithm name

--- AppDownloaderLib/src/main/java/net/dankito/appdownloader/app/HashAlgorithm.java
+++ AppDownloaderLib/src/main/java/net/dankito/appdownloader/app/HashAlgorithm.java
@@ -6,8 +6,10 @@ package net.dankito.appdownloader.app;
 
 public enum HashAlgorithm {
 
+  // for supported algorithms see: https://developer.android.com/reference/java/security/MessageDigest.html
+
   MD5("MD5"),
-  SHA1("SHA");
+  SHA1("SHA-1");
 
 
   private String algorithmName;
