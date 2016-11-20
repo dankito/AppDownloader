39ad920e2b8020c7f690b26558e01754be9fa17d
dankito <dankito@user.noreply.github.com>
2016-11-20 15:36:32 +0100

    Implemented returning download url variants

--- AppDownloaderLib/src/main/java/net/dankito/appdownloader/downloader/util/ApkLeecherDownloadLinkGenerator.java
+++ AppDownloaderLib/src/main/java/net/dankito/appdownloader/downloader/util/ApkLeecherDownloadLinkGenerator.java
@@ -3,6 +3,9 @@ package net.dankito.appdownloader.downloader.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * As for downloading from ApkLeecher JavaScript must be executed.
  * So we try to generate download link manually.
@@ -22,8 +25,16 @@ public class ApkLeecherDownloadLinkGenerator {
   }
 
 
-  public String generateDownloadUrl() {
+  /**
+   * The actual download url is not absolute certain.
+   * Sometimes it contains www.apkleecher.com, sometimes www.Apkleecher.com.
+   * Sometimes there's a underscore at the end of the app name, mostly not.
+   * @return
+   */
+  public List<String> generateDownloadUrlVariants() {
     try {
+      List<String> downloadUrlVariants = new ArrayList<>();
+
       int indexAppNameAndVersionSeparator = appNameAndVersion.lastIndexOf('.'); // sometimes there's a string at the end of the version, e.g. ' lite'
       indexAppNameAndVersionSeparator = appNameAndVersion.lastIndexOf(' ', indexAppNameAndVersionSeparator);
 
@@ -34,7 +45,12 @@ public class ApkLeecherDownloadLinkGenerator {
 
       String formattedVersion = version.replace(" ", "%20");
 
-      return "http://apkleecher.com/apps/" + formattedUpdateString + "/" + appName + formattedVersion + "_[www.apkleecher.com].apk";
+      String urlBase = "http://apkleecher.com/apps/" + formattedUpdateString + "/" + appName;
+
+      downloadUrlVariants.add(urlBase + formattedVersion + "_[www.apkleecher.com].apk");
+      downloadUrlVariants.add(urlBase + formattedVersion + "_[www.Apkleecher.com].apk");
+
+      return downloadUrlVariants;
     } catch(Exception e) {
       log.error("Could not generate download link", e);
     }
--- AppDownloaderLib/src/test/java/net/dankito/appdownloader/downloader/util/ApkLeecherDownloadLinkGeneratorTest.java
+++ AppDownloaderLib/src/test/java/net/dankito/appdownloader/downloader/util/ApkLeecherDownloadLinkGeneratorTest.java
@@ -35,7 +35,12 @@ public class ApkLeecherDownloadLinkGeneratorTest {
 //    testDownloadUrlGeneration("BusyBox Install (No Root) 3.66.0.41", "2016-06-16", "http://apkleecher.com/apps/2016/06/16/BusyBox_Install_No_Root_%203.66.0.41_[www.Apkleecher.com].apk");
     testDownloadUrlGeneration("BusyBox Install (No Root) 3.66.0.41", "2016-06-16", "http://apkleecher.com/apps/2016/06/16/BusyBox_Install_No_Root_%203.66.0.41_[www.apkleecher.com].apk");
 
-    testDownloadUrlGeneration("VPN Master(Free unblock proxy) 5.2.1", "2016-11-20", "http://apkleecher.com/apps/2016/11/20/VPN_Master_Free_unblock_proxy%205.2.1_[www.apkleecher.com].apk");
+//    testDownloadUrlGeneration("Wifi Password(ROOT) 1.5.2", "2016-06-22", "http://apkleecher.com/apps/2016/06/22/Wifi_Password_ROOT_%201.5.2_[www.Apkleecher.com].apk");
+    testDownloadUrlGeneration("Wifi Password(ROOT) 1.5.2", "2016-06-22", "http://apkleecher.com/apps/2016/06/22/Wifi_Password_ROOT_%201.5.2_[www.apkleecher.com].apk");
+
+    testDownloadUrlGeneration("Device Control [root] 0.8.7.2-git-fb92bc", "2016-11-19", "http://apkleecher.com/apps/2016/11/19/Device_Control_root_%200.8.7.2-git-fb92bc_[www.apkleecher.com].apk");
+
+    testDownloadUrlGeneration("L Speed [ROOT] 1.3.9.12", "2016-11-04", "http://apkleecher.com/apps/2016/11/04/L_Speed_ROOT_%201.3.9.12_[www.apkleecher.com].apk");
 
     testDownloadUrlGeneration("VPN Master(Free unblock proxy) 5.2.1", "2016-11-20", "http://apkleecher.com/apps/2016/11/20/VPN_Master_Free_unblock_proxy%205.2.1_[www.apkleecher.com].apk");
   }
@@ -46,6 +51,8 @@ public class ApkLeecherDownloadLinkGeneratorTest {
     testDownloadUrlGeneration("VideoShowLite: Video editor 7.1.7 lite", "2016-11-20", "http://apkleecher.com/apps/2016/11/20/VideoShowLite_Video_editor%207.1.7%20lite_[www.apkleecher.com].apk");
 
     testDownloadUrlGeneration("VivaVideo: Free Video Editor 5.3.0", "2016-11-20", "http://apkleecher.com/apps/2016/11/20/VivaVideo_Free_Video_Editor%205.3.0_[www.apkleecher.com].apk");
+
+    testDownloadUrlGeneration("Titanium Backup ★ root 7.5.0.3", "2016-11-19", "http://apkleecher.com/apps/2016/11/19/Titanium_Backup_root%207.5.0.3_[www.apkleecher.com].apk");
   }
 
 
@@ -53,7 +60,7 @@ public class ApkLeecherDownloadLinkGeneratorTest {
     underTest.setAppNameAndVersion(appName);
     underTest.setLastUpdatedOn(lastUpdatedOn);
 
-    Assert.assertEquals(correctDownloadLink, underTest.generateDownloadUrl());
+    Assert.assertTrue(underTest.generateDownloadUrlVariants().contains(correctDownloadLink));
   }
 
 }
