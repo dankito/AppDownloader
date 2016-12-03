825eab7a8ac042a587b6e63cfe7897d1df353df8
dankito <dankito@user.noreply.github.com>
2016-12-03 22:40:58 +0100

    Forgot to check in AppPackageVerificationCallback

---
+++ AppDownloaderLib/src/main/java/net/dankito/appdownloader/app/AppPackageVerificationCallback.java
@@ -0,0 +1,11 @@
+package net.dankito.appdownloader.app;
+
+/**
+ * Created by ganymed on 03/12/16.
+ */
+
+public interface AppPackageVerificationCallback {
+
+  void completed(AppPackageVerificationResult result);
+
+}
