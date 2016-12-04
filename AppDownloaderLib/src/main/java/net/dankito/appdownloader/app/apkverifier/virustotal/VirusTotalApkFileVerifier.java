c1a83ac04d22986bb0c0041cb9c07537d668f0dd
dankito <dankito@user.noreply.github.com>
2016-12-04 16:36:02 +0100

    Implemented when a file is not known to VirusTotal, uploading it and periodically checking if file scan report is available

--- AppDownloaderLib/src/main/java/net/dankito/appdownloader/app/apkverifier/virustotal/VirusTotalApkFileVerifier.java
+++ AppDownloaderLib/src/main/java/net/dankito/appdownloader/app/apkverifier/virustotal/VirusTotalApkFileVerifier.java
@@ -4,13 +4,19 @@ import net.dankito.appdownloader.app.apkverifier.DownloadedApkInfo;
 import net.dankito.appdownloader.app.apkverifier.IApkFileVerifier;
 import net.dankito.appdownloader.app.apkverifier.VerifyApkFileCallback;
 import net.dankito.appdownloader.app.apkverifier.VerifyApkFileResult;
-import net.dankito.appdownloader.app.apkverifier.virustotal.calback.GetFileScanCallback;
-import net.dankito.appdownloader.app.apkverifier.virustotal.response.GetFileScanResponse;
+import net.dankito.appdownloader.app.apkverifier.virustotal.calback.GetFileScanReportCallback;
+import net.dankito.appdownloader.app.apkverifier.virustotal.calback.ScanFileCallback;
+import net.dankito.appdownloader.app.apkverifier.virustotal.response.GetFileScanReportResponse;
+import net.dankito.appdownloader.app.apkverifier.virustotal.response.ScanFileResponse;
 import net.dankito.appdownloader.app.apkverifier.virustotal.response.VirusTotalFileScanReportResponse;
+import net.dankito.appdownloader.app.apkverifier.virustotal.response.VirusTotalFileScanResponse;
+import net.dankito.appdownloader.app.model.AppState;
 import net.dankito.appdownloader.util.web.IWebClient;
 
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by ganymed on 22/11/16.
@@ -19,6 +25,8 @@ import java.util.Properties;
 public class VirusTotalApkFileVerifier implements IApkFileVerifier {
 
   public static final int MAX_NUMBER_OF_POSITIVE_FILE_SCANS_TO_JUDGE_SECURE = 2;
+
+  protected static final int CHECK_IF_FILE_SCAN_REPORT_IS_AVAILABLE_PERIOD_MILLIS = 60 * 1000; // every minute
 
 
   private static final String VIRUS_TOTAL_PROPERTIES_FILENAME = "virus_total.properties";
@@ -40,9 +48,9 @@ public class VirusTotalApkFileVerifier implements IApkFileVerifier {
   public void verifyApkFile(final DownloadedApkInfo downloadedApkInfo, final VerifyApkFileCallback callback) {
     final FileScanRequest request = new FileScanRequest(downloadedApkInfo.getDownloadSource().getDownloadLocationPath(), downloadedApkInfo.getSha256CheckSum(), "application/vnd.android.package-archive");
 
-    virusTotalClient.getFileScanReportAsync(request, new GetFileScanCallback() {
+    virusTotalClient.getFileScanReportAsync(request, new GetFileScanReportCallback() {
       @Override
-      public void completed(GetFileScanResponse response) {
+      public void completed(GetFileScanReportResponse response) {
         if(response.isSuccessful() == false) {
           callback.completed(new VerifyApkFileResult(downloadedApkInfo, response.getError())); // TODO: check responseCode, e.g. if API limit exceeded etc.
         }
@@ -58,7 +66,7 @@ public class VirusTotalApkFileVerifier implements IApkFileVerifier {
     });
   }
 
-  protected VerifyApkFileResult createVerifyApkFileResultFromFileScanReport(GetFileScanResponse response, DownloadedApkInfo downloadedApkInfo) {
+  protected VerifyApkFileResult createVerifyApkFileResultFromFileScanReport(GetFileScanReportResponse response, DownloadedApkInfo downloadedApkInfo) {
     VirusTotalFileScanReportResponse fileScanReportResponse = response.getResponse();
     boolean knowsApkFile = fileScanReportResponse.getTotal() > 0; // response successful and at least one file scan
     boolean isSecure = fileScanReportResponse.getPositives() < MAX_NUMBER_OF_POSITIVE_FILE_SCANS_TO_JUDGE_SECURE; // sometimes there are false positives
@@ -66,8 +74,48 @@ public class VirusTotalApkFileVerifier implements IApkFileVerifier {
     return new VerifyApkFileResult(downloadedApkInfo, knowsApkFile, isSecure, true);
   }
 
-  protected void startFileScan(DownloadedApkInfo downloadedApkInfo, FileScanRequest request, VerifyApkFileCallback callback) {
+  protected void startFileScan(final DownloadedApkInfo downloadedApkInfo, final FileScanRequest request, final VerifyApkFileCallback callback) {
+    virusTotalClient.scanFileAsync(request, new ScanFileCallback() {
+      @Override
+      public void completed(ScanFileResponse response) {
+        if(response.isSuccessful() == false) {
+          callback.completed(new VerifyApkFileResult(downloadedApkInfo, response.getError()));
+        }
+        else {
+          periodicallyCheckIfScanReportAvailable(downloadedApkInfo, request, response, callback);
+        }
+      }
+    });
+  }
 
+  protected void periodicallyCheckIfScanReportAvailable(final DownloadedApkInfo downloadedApkInfo, final FileScanRequest request, ScanFileResponse response, final VerifyApkFileCallback callback) {
+    downloadedApkInfo.getApp().setState(AppState.SCANNING_FOR_VIRUSES);
+
+    VirusTotalFileScanResponse scanResponse = response.getResponse();
+    request.setSha256Checksum(scanResponse.getSha256());
+    request.setScanId(scanResponse.getScan_id());
+
+    final Timer checkIfScanReportAvailableTimer = new Timer();
+    checkIfScanReportAvailableTimer.schedule(new TimerTask() {
+      @Override
+      public void run() {
+        checkIfFileScanReportIsAvailable(downloadedApkInfo, request, checkIfScanReportAvailableTimer, callback);
+      }
+    }, CHECK_IF_FILE_SCAN_REPORT_IS_AVAILABLE_PERIOD_MILLIS, CHECK_IF_FILE_SCAN_REPORT_IS_AVAILABLE_PERIOD_MILLIS);
+  }
+
+  protected void checkIfFileScanReportIsAvailable(final DownloadedApkInfo downloadedApkInfo, FileScanRequest request, final Timer checkIfScanReportAvailableTimer, final VerifyApkFileCallback callback) {
+    virusTotalClient.getFileScanReportAsync(request, new GetFileScanReportCallback() {
+      @Override
+      public void completed(GetFileScanReportResponse response) {
+        if(response.isSuccessful() && response.getResponseCode() == ResponseCode.ITEM_FOUND) { // TODO: implement other response codes
+          checkIfScanReportAvailableTimer.cancel();
+          checkIfScanReportAvailableTimer.purge();
+
+          callback.completed(createVerifyApkFileResultFromFileScanReport(response, downloadedApkInfo));
+        }
+      }
+    });
   }
 
 
--- AppDownloaderLib/src/test/java/net.dankito.appdownloader/app/apkverifier/virustotal/VirusTotalApkFileVerifierTest.java
+++ AppDownloaderLib/src/test/java/net.dankito.appdownloader/app/apkverifier/virustotal/VirusTotalApkFileVerifierTest.java
@@ -3,13 +3,22 @@ package net.dankito.appdownloader.app.apkverifier.virustotal;
 import net.dankito.appdownloader.app.apkverifier.DownloadedApkInfo;
 import net.dankito.appdownloader.app.apkverifier.VerifyApkFileCallback;
 import net.dankito.appdownloader.app.apkverifier.VerifyApkFileResult;
+import net.dankito.appdownloader.app.model.AppDownloadInfo;
 import net.dankito.appdownloader.app.model.AppInfo;
+import net.dankito.appdownloader.app.model.AppState;
+import net.dankito.appdownloader.util.apps.TestAppDownloader;
 import net.dankito.appdownloader.util.web.OkHttpWebClient;
 
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
@@ -20,27 +29,45 @@ import java.util.concurrent.TimeUnit;
  */
 public class VirusTotalApkFileVerifierTest {
 
+  protected static final String AUTO_START_APP_FILENAME = "AutoStart.apk";
+
   protected static final String AUTO_START_SHA_256 = "be805cbbfacf5ab11313b287104a51db28dada359042799e2dd03cc7898be88c";
 
   protected static final String NO_ROOT_FIREWALL_SHA_256 = "b8c7e4fd106c3be8fa0cf02d4d1ff805b9e858f8328fa0f88ca1b91581323bc5";
 
 
+  protected static String testApkFilePath = null;
+
+
   protected VirusTotalApkFileVerifier underTest;
 
 
+
+  @BeforeClass
+  public static void provideTestApks() throws Exception {
+    testApkFilePath = copyFromResourcesToReadableDirectory(AUTO_START_APP_FILENAME).getAbsolutePath();
+  }
+
+  @AfterClass
+  public static void deleteTestApks() throws Exception {
+    if(testApkFilePath != null) {
+      deleteFile(testApkFilePath);
+    }
+  }
+
+
   @Before
-  public void setUp() {
+  public void setUp() throws Exception {
     underTest = new VirusTotalApkFileVerifier(new OkHttpWebClient());
   }
 
 
   @Test
   public void checkKnownFile_Succeeds() throws Exception {
+    DownloadedApkInfo downloadedApkInfo = createTestDownloadedApkInfo();
+
     final List<VerifyApkFileResult> resultList = new ArrayList<>();
     final CountDownLatch countDownLatch = new CountDownLatch(1);
-
-    DownloadedApkInfo downloadedApkInfo = new DownloadedApkInfo(new AppInfo(""), null, "");
-    downloadedApkInfo.setSha256CheckSum(NO_ROOT_FIREWALL_SHA_256);
 
     underTest.verifyApkFile(downloadedApkInfo, new VerifyApkFileCallback() {
       @Override
@@ -50,7 +77,7 @@ public class VirusTotalApkFileVerifierTest {
       }
     });
 
-    try { countDownLatch.await(5, TimeUnit.MINUTES); } catch(Exception ignored) { }
+    try { countDownLatch.await(30, TimeUnit.SECONDS); } catch(Exception ignored) { }
 
     Assert.assertEquals(1, resultList.size());
 
@@ -61,11 +88,80 @@ public class VirusTotalApkFileVerifierTest {
     Assert.assertTrue(result.isFileChecksumFromIndependentSource());
   }
 
-//  @Test
-//  public void fileNotKnownToVirusTotal_Fails() throws Exception {
-//    boolean result = underTest.verifyApkFile(new AppInfo(""), "", "", "a" + NO_ROOT_FIREWALL_SHA_256.substring(1)); // fake a SHA256
-//
-//    Assert.assertFalse(result);
-//  }
+  @Test
+  public void fileNotKnownToVirusTotal_ScansForVirusesOnline() throws Exception {
+    DownloadedApkInfo downloadedApkInfo = createTestDownloadedApkInfo();
+    downloadedApkInfo.setSha256CheckSum("a" + NO_ROOT_FIREWALL_SHA_256.substring(1)); // generate fake SHA256 checksum
+
+    Assert.assertNotEquals(AppState.SCANNING_FOR_VIRUSES, downloadedApkInfo.getApp().getState());
+
+    final List<VerifyApkFileResult> resultList = new ArrayList<>();
+    final CountDownLatch countDownLatch = new CountDownLatch(1);
+
+    underTest.verifyApkFile(downloadedApkInfo, new VerifyApkFileCallback() {
+      @Override
+      public void completed(VerifyApkFileResult result) {
+        resultList.add(result);
+        countDownLatch.countDown();
+      }
+    });
+
+    try { countDownLatch.await(15, TimeUnit.MINUTES); } catch(Exception ignored) { }
+
+
+    Assert.assertEquals(AppState.SCANNING_FOR_VIRUSES, downloadedApkInfo.getApp().getState());
+
+    Assert.assertEquals(1, resultList.size());
+
+    VerifyApkFileResult result = resultList.get(0);
+    Assert.assertTrue(result.isSuccessful());
+    Assert.assertTrue(result.knowsFileChecksum());
+    Assert.assertTrue(result.couldVerifyFileChecksum());
+    Assert.assertTrue(result.isFileChecksumFromIndependentSource());
+  }
+
+
+
+  protected DownloadedApkInfo createTestDownloadedApkInfo() {
+    AppInfo appInfo = new AppInfo("");
+
+    AppDownloadInfo downloadInfo = new AppDownloadInfo(appInfo, new TestAppDownloader());
+    downloadInfo.setDownloadLocationPath(testApkFilePath);
+
+    DownloadedApkInfo downloadedApkInfo = new DownloadedApkInfo(appInfo, downloadInfo, "");
+    downloadedApkInfo.setSha256CheckSum(NO_ROOT_FIREWALL_SHA_256);
+
+    return downloadedApkInfo;
+  }
+
+
+  protected static File copyFromResourcesToReadableDirectory(String resourceFilename) throws Exception {
+    InputStream inputStream = VirusTotalApkFileVerifierTest.class.getClassLoader().getResourceAsStream(resourceFilename);
+    File destinationFile = File.createTempFile(resourceFilename, ".apk");
+
+    OutputStream fileOutputStream = new FileOutputStream(destinationFile);
+    byte[] buffer = new byte[8 * 1024];
+    int read = 0;
+
+    while((read = inputStream.read(buffer, 0, buffer.length)) > 0) {
+      fileOutputStream.write(buffer, 0, read);
+    }
+
+    fileOutputStream.flush();
+    fileOutputStream.close();
+    inputStream.close();
+
+    return destinationFile;
+  }
+
+  protected static void deleteFile(String filePath) {
+    try {
+      File file = new File(filePath);
+      if(file.exists()) {
+        file.delete();
+      }
+    } catch(Exception e) {
+    }
+  }
 
 }
